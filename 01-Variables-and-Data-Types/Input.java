073ced8b6b3be3f39e288f2fedf0921f40c45b95
Tushar-Joshi-10 <tusharjoshi7478@gmail.com>
2026-08-21 17:44:04 +0530

    Add Input in Java and sum, multiplication and area of circle

--- 01-Variables-and-Data-Types/Input.java
+++ 01-Variables-and-Data-Types/Input.java
@@ -11,13 +11,31 @@ public class Input {
         System.out.println(name);
         
         int num1 = sc.nextInt();
-        System.out.println(name);
+        System.out.println(num1);
         
         float num2 = sc.nextFloat();
-        System.out.println(name);
+        System.out.println(num2);
 
         //nextByte, nextDouble, nextShort, nextLong and many more... 
 
+        //SUM OF a AND b (INPUT FROM USER)
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
 
+        System.out.println(sum);
+
+        //Multiplication OF A AND B (INPUT FROM USER)
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int mul = A * B;
+
+        System.out.println(mul);
+
+        //Area of circle
+        float radius = sc.nextFloat();
+        float area = 3.14f * radius * radius; //3.14 -> double, but 3.14f -> float
+        
+        System.out.println(area);
     }
 }
